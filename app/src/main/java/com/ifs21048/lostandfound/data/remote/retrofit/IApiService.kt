b339c0696e4b18bc0962521706d60fd0d5f375fd
11S21048 <ifs21048@students.del.ac.id>
2024-04-25 15:24:15 +0700

    PR 4 Fitur Add Cover
    Menambahkan fitur edit cover

--- app/src/main/java/com/ifs21048/lostandfound/data/remote/retrofit/IApiService.kt
+++ app/src/main/java/com/ifs21048/lostandfound/data/remote/retrofit/IApiService.kt
@@ -6,12 +6,15 @@ import com.ifs21048.lostandfound.data.remote.response.DelcomLostandFoundResponse
 import com.ifs21048.lostandfound.data.remote.response.DelcomLostandFoundsResponse
 import com.ifs21048.lostandfound.data.remote.response.DelcomResponse
 import com.ifs21048.lostandfound.data.remote.response.DelcomUserResponse
+import okhttp3.MultipartBody
 import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Part
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -67,5 +70,12 @@ interface IApiService {
     @DELETE("lost-founds/{id}")
     suspend fun deleteLostandFound(
         @Path("id") lostandfoundId: Int
+    ): DelcomResponse
+
+    @Multipart
+    @POST("lost-founds/{id}/cover")
+    suspend fun addCoverLostandFound(
+        @Path("id") lostandfoundId: Int,
+        @Part cover: MultipartBody.Part,
     ): DelcomResponse
 }
